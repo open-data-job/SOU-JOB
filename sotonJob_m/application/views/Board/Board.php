fd086b5991ad7fc25216128bc928aa31bfe6723c
Daniel Deng <xd1u18@soton.ac.uk>
2019-03-25 09:30:16 +0800

    Revert "SoJ Project(php+mysql+(low)font)"
    This reverts commit 8af3cb30727c4a1d35266824e45d9f37ab7c0ef6.

--- sotonJob_m/application/views/Board/Board.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>公文通页面</title>
-</head>
-<body>
-	<h1>公文通页面</h1>
-</body>
-</html>
--- sotonJob_m/test-cURL/test.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	$curl = curl_init("www.baidu.com");
-	curl_exec($curl);
-	curl_close($curl);
- ?>
